1bf9800c3a2ed0099f7597217f1408d6eb71e60e
Thiago Cabral <thiagocabral477@gmail.com>
2021-04-01 07:20:23 -0300

    adionado rota allTasks

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
 import authenticated from '../middlewares/authenticated'
+import AllTasksService from '../services/allTasksService'
 import CreateTaskService from '../services/CreateTaskService'
 
 const tasksRouter = Router()
 
 tasksRouter.use(authenticated)
+
+tasksRouter.get('/', async (request, response) => {
+  const { id_user } = request.body
+
+  const getAllTasks = new AllTasksService()
+
+  const tasks = await getAllTasks.execute(id_user)
+
+  return response.json(tasks)
+})
 
 tasksRouter.post('/', async (request, response) => {
   const { id_user, tasks, deadline, status } = request.body
